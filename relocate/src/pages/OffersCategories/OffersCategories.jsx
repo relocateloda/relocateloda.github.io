05e9c312fda3b951a6f4a92b21cad70482fd56d8
Taras <tarasbatyuchok@gmail.com>
2022-04-07 20:17:48 +0300

    Offers

---
+++ relocate/src/pages/OffersCategories/OffersCategories.jsx
@@ -0,0 +1,12 @@
+import styles from "./OffersCategories.module.scss";
+
+
+const MainPage = () => {
+  return (
+    <div className={styles.container}>
+     
+    </div>
+  );
+};
+
+export default MainPage;
