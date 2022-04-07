7f2052d6a837f3ddcfadfa199d25cb4eea13bd3c
ihor.kurylov@clikk.app <idSNrY7I3xhB>
2022-04-07 08:21:44 +0300

    navigation from main page

--- relocate/src/pages/Main/Mainpage.jsx
+++ relocate/src/pages/Main/Mainpage.jsx
@@ -1,35 +1,37 @@
 import styles from "./Mainpage.module.scss";
-
+import { useNavigate } from "react-router-dom";
 import relocation from "../../assets/icon/location-pin.png";
 import currency from "../../assets/icon/currency.png";
-import arrow from "../../assets/icon/arrow.png"
-import Navbar from "../../Atom/Navbar/Navbar"
+import arrow from "../../assets/icon/arrow.png";
+import Navbar from "../../Atom/Navbar/Navbar";
+import { RoutesConst } from "../../common/Routes";
 
 const MainPage = () => {
+    const navigate = useNavigate();
   return (
     <div className={styles.container}>
       <span>
-      <Navbar/>
+        <Navbar />
       </span>
       <div className={styles.wrapper}>
-      <div className={styles.cardOne}>
-      <img className={styles.icon}src={relocation} alt="car" />
-        <h2 className={styles.title}>Ви релоковане підприємство?</h2>
-        <div className={styles.subtitle}>
-          переглянути пропозицію
-          <img className={styles.arrow} src={arrow} alt="arrow" />
+        <div className={styles.cardOne} onClick={()=>navigate(RoutesConst.OFFERS)}>
+          <img className={styles.icon} src={relocation} alt="car" />
+          <h2 className={styles.title}>Ви релоковане підприємство?</h2>
+          <div className={styles.subtitle}>
+            переглянути пропозицію
+            <img className={styles.arrow} src={arrow} alt="arrow" />
           </div>
-      </div>
-      <div className={styles.cardSecond}>
-        <img className={styles.icon} src={currency} alt="car" />
-        <h2 className={styles.title}>Ви існуючий бізнес?</h2>
-        <div className={styles.subtitle}>
-          подати ипропозицію
-          <img className={styles.arrow} src={arrow} alt="arrow" />
+        </div>
+        <div className={styles.cardSecond} onClick={()=>navigate(RoutesConst.SEND_OFFER)}>
+          <img className={styles.icon} src={currency} alt="car" />
+          <h2 className={styles.title}>Ви існуючий бізнес?</h2>
+          <div className={styles.subtitle}>
+            подати ипропозицію
+            <img className={styles.arrow} src={arrow} alt="arrow" />
           </div>
+        </div>
       </div>
-      </div>
-      </div>
+    </div>
   );
 };
 
