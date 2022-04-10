be2c17aaf1f1fd9c66dc07603f6af4bd45ec4f4f
ihor.kurylov@clikk.app <idSNrY7I3xhB>
2022-04-10 11:05:34 +0300

    init admin edit page

--- relocate/src/Atom/Navbar/Navbar.jsx
+++ relocate/src/Atom/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import styles from "./Navbar.module.scss";
-import logo from "../../assets/logo.png"
-
+import logo from "../../assets/logo.png";
+import { RoutesConst } from "../../common/Routes";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <div className={styles.navbar}>
-            <img className={styles.logo} src={logo} />
-      <p className={styles.title}>Львівська Обласна Державна Адміністрація</p> 
+    <div className={styles.navbar} onClick={() => navigate(RoutesConst.MAIN)}>
+      <img className={styles.logo} src={logo} />
+      <p className={styles.title}>Львівська Обласна Державна Адміністрація</p>
     </div>
   );
 };
-
 
 export default Navbar;
--- relocate/src/pages/AdminProposals/AdminProposals.jsx
+++ relocate/src/pages/AdminProposals/AdminProposals.jsx
@@ -1,11 +1,20 @@
 import { encryptStorageAdmin } from "../../utils/encryptStorage";
-
+import styles from "./AdminProposals.module.scss"
+import Navbar from "../../Atom/Navbar/Navbar";
 
 const AdminProposals = () => {
-    encryptStorageAdmin.setItem("au", false);
     return (
-        <div>
-            <h1>Admin PAGE WITH Proposals</h1>
+        <div className={styles.container}>
+            <Navbar />
+            <h1 className={styles.headerText}>Редагування погоджених та неперевірених пропозицій</h1>
+            <div className={styles.flexBox}>
+                <div>
+                    <h3>Редагування погоджених пропозицій</h3>
+                </div>
+                <div>
+                    <h3>Редагування неперевірених пропозицій</h3>
+                </div>
+            </div>
         </div>
     )
 }
